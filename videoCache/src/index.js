e627aba28fa3ab4bf64a45980ac09fdd5461487e
J. G. Manzano <unico.javier@gmail.com>
2017-02-23 19:17:30 +0100

    Prueba JAvi

--- videoCache/src/index.js
+++ videoCache/src/index.js
@@ -6,7 +6,7 @@ nameFile = process.argv[3];
 if(nameFile != null){
 	nameFile = process.env.NFILE;
 }else{
-	nameFile = 'example.in';
+	nameFile = 'examples/kittens.in';
 }
 //const nameFile = require('nameFile');
 
